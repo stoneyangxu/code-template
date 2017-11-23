c5e6e3bcb886417d28656fe15a401c206f93c6e1
stoneyangxu <stoneyangxu@icloud.com>
2017-11-24 02:25:32 +0800

    add eslint

--- src/generator/gen-editorconfig.js
+++ src/generator/gen-editorconfig.js
@@ -1,23 +1,23 @@
+import { error, info } from '../utils/log';
 
-const path = require('path')
-const fs = require('fs')
-const chalk = require('chalk');
+const path = require('path');
+const fs = require('fs');
 
-const templateFile = path.resolve(__dirname, '../../boilerplates', '.editorconfig')
+const templateFile = path.resolve(__dirname, '../../boilerplates', '.editorconfig');
 
-export default function(destPath = '.') {
-  const absolutePath = path.resolve(destPath)
+export default function (destPath = '.') {
+  const absolutePath = path.resolve(destPath);
 
   if (!fs.existsSync(absolutePath)) {
-    console.log(chalk.red(`target path not exist: ${absolutePath}`))
-    return
+    error(`target path not exist: ${absolutePath}`);
+    return;
   }
 
   if (!fs.existsSync(templateFile)) {
-    console.log(chalk.red(`template file not exist: ${templateFile}`))
+    error(`template file not exist: ${templateFile}`);
   }
 
-  console.log(chalk.green(`generate .editorconfig at ${absolutePath}`))
+  info(`generate .editorconfig at ${absolutePath}`);
 
-  fs.copyFileSync(templateFile, path.resolve(absolutePath, '.editorconfig'))
+  fs.copyFileSync(templateFile, path.resolve(absolutePath, '.editorconfig'));
 }
---
+++ src/utils/log.js
@@ -0,0 +1,19 @@
+
+const chalk = require('chalk');
+
+function log(...args) {
+  // eslint-disable-next-line no-console
+  console.log(chalk.gray(args));
+}
+
+function info(...args) {
+  // eslint-disable-next-line no-console
+  console.log(chalk.green(args));
+}
+
+function error(...args) {
+  // eslint-disable-next-line no-console
+  console.log(chalk.red(args));
+}
+
+export { log, info, error };
