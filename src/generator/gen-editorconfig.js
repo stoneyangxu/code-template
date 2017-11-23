9df458bf4c9bb5903447a8ea12c6dd4a29e5f7fe
stoneyangxu <stoneyangxu@icloud.com>
2017-11-24 02:07:57 +0800

    use current dir as default

--- src/generator/gen-editorconfig.js
+++ src/generator/gen-editorconfig.js
@@ -5,13 +5,8 @@ const chalk = require('chalk');
 
 const templateFile = path.resolve(__dirname, '../../boilerplates', '.editorconfig')
 
-export default function(destPath) {
-  let absolutePath = '';
-  if (destPath) {
-    absolutePath = path.resolve(destPath)
-  } else {
-    absolutePath = __dirname
-  }
+export default function(destPath = '.') {
+  const absolutePath = path.resolve(destPath)
 
   if (!fs.existsSync(absolutePath)) {
     console.log(chalk.red(`target path not exist: ${absolutePath}`))
@@ -21,6 +16,8 @@ export default function(destPath) {
   if (!fs.existsSync(templateFile)) {
     console.log(chalk.red(`template file not exist: ${templateFile}`))
   }
+
+  console.log(chalk.green(`generate .editorconfig at ${absolutePath}`))
 
   fs.copyFileSync(templateFile, path.resolve(absolutePath, '.editorconfig'))
 }
