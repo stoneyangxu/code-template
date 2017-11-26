f9e6a8e5ea25e9b0f07d6abbfd244a704d58b0dd
stoneyangxu <stoneyangxu@icloud.com>
2017-11-27 00:56:13 +0800

    add add coverage report

--- src/generator/cmd-types.js
+++ src/generator/cmd-types.js
@@ -1,3 +1,4 @@
 export const TEST = 'test';
 export const EDITORCONFIG = 'editorconfig';
 export const REDUX_THUNK_STORE = 'redux-thunk-store';
+export const REDUX_INDEX = 'redux-index';
--- src/generator/generator-path.js
+++ src/generator/generator-path.js
@@ -17,4 +17,9 @@ export default [
     path: buildPath('redux/store.js'),
     desc: 'Basic store.js base on redux and redux-thunk',
   },
+  {
+    cmd: types.REDUX_INDEX,
+    path: buildPath('redux/index.js'),
+    desc: 'Basic index.js base on existing store and create-react-app',
+  },
 ];
